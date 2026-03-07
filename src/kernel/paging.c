62ea5e4cb3ff3251622e14acaf062c9b813f2c64
abhilashdharpure <abhilashdharpure@gmail.com>
2026-03-07 22:00:37 +0100

    Update

--- src/kernel/paging.c
+++ src/kernel/paging.c
@@ -426,7 +426,8 @@ void enter_user_mode_from_process(Process *p)
 
     log_info("EXEC", "enter_user_mode_from_process: after write_cr3, jumping to user");
 
-    enter_user_mode(p);
+    // enter_user_mode(p);
+    enter_user_mode(&p->regs);
 
 
     log_critical("EXEC", "enter_user_mode: returned unexpectedly from user mode");
